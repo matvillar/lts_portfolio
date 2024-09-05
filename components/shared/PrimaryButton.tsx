3f38439a0534a978d1152a35b1b9a4e886301bde
Matias Villar <matiase.villar@gmail.com>
2024-09-05 14:27:36 -0600

    resume pdf link

--- components/shared/PrimaryButton.tsx
+++ components/shared/PrimaryButton.tsx
@@ -1,3 +1,4 @@
+'use client';
 import React from 'react';
 import { Button } from '@/components/ui/button';
 
@@ -21,6 +22,7 @@ const PrimaryButton = ({
   }
   return (
     <Button
+      onClick={handleClick}
       className={`${padding} w-full lg:w-[40%] border-2 font-semibold tracking-wider border-white rounded-lg transition-all hover:-translate-x-1 hover:-translate-y-1 hover:shadow-[4px_4px_0px_0px_rgba(139,92,246,1)]`}
     >
       {Icon && <Icon size={24} className="mr-2" />} {title}
